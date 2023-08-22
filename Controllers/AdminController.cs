3741d1d40a577eef74886dd76396392cb586ab40
Ramakrishna <pramakrishna49@gmail.com>
2023-08-22 22:00:32 +0530

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,92 @@
+﻿using DSR_DataAccess.Models;
+using DSR_DataAccess.Services;
+using DSR_DataAccess.Utils.CommonModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace DSR.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        IAdminService _adminService;
+        public AdminController(IAdminService adminService)
+        {
+            _adminService=adminService;
+        }
+
+        [HttpPost]
+        public SuccessResponse AddHotel(Hotel hotel)
+        {
+            var result = _adminService.AddHotels(hotel);
+            return result;
+        }
+        [HttpPost]
+        public SuccessResponse AddRoom(Room room)
+        {
+            var result = _adminService.AddRooms(room);
+            return result;
+        }
+
+        [HttpPost]
+        public SuccessResponse AddAmendment(Amendment amendment)
+        {
+            var result = _adminService.AddAmendments(amendment);
+            return result;
+        }
+        [HttpPost]
+        public SuccessResponse AddRating(Rating rating)
+        {
+            var result = _adminService.AddRatings(rating);
+            return result;
+        }
+
+        [HttpPost]
+        public SuccessResponse AddContactDetails(ContactDetail contactDetail)
+        {
+            var result = _adminService.AddContactDetails(contactDetail);
+            return result;
+        }
+
+        [HttpPost]
+        public SuccessResponse AddLocation(Location location)
+        {
+            var result = _adminService.AddLocations(location);
+            return result;
+        }
+        [HttpPost]
+        public SuccessResponse AddAdditionalGuestCost(AdditionalGuestCost additionalGuestCost)
+        {
+            var result = _adminService.AddAdditionalGuestCost(additionalGuestCost);
+            return result;
+        }
+
+        [HttpPost]
+        public SuccessResponse AddUser(User user)
+        {
+            var result = _adminService.AddUsers(user);
+            return result;
+        }
+
+        [HttpPost]
+        public SuccessResponse AddImage(Image image)
+        {
+            var result = _adminService.AddImages(image);
+            return result;
+        }
+
+        [HttpPost]
+        public SuccessResponse AddAdmins(Dsradmin dsradmin)
+        {
+            var result = _adminService.AddAdmin(dsradmin);
+            return result;
+        }
+
+        [HttpGet]
+        public List<Hotel>  GetHotels(int? hotelid)
+        {
+            var result=_adminService.GetHotels(hotelid);
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+
+using DSR_DataAccess.Services;
+using DSR_DataAccess.Services.CommonServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//adding servicess
+builder.Services.AddTransient<IAdminService,AdminServices>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
